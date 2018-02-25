9c89e75fa29581d65a24920627a7d835aeb30335
Noah Brown <nebrown@ucsc.edu>
2018-02-25 13:40:25 -0800

    Fixed atomic number

--- models/Atom.js
+++ models/Atom.js
@@ -38,6 +38,15 @@ var atomicRadii = [[53,null,null,null,null,null,null,null,null,null,null,null,nu
                      [null,null,null,0, 247, 206, 205, 238, 231, 233, 225, 228, 226, 226, 222, 222, 217,null],
                      [null,null,null,0,0,0,0,0,0,0,0,0,0,0,0,0,0,null]];
 
+var atomicNumber = [[1,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,2],
+                     [3, 4,null,null,null,null,null,null,null,null,null,null, 5, 6, 7, 8, 9, 10],
+                     [11, 12,null,null,null,null,null,null,null,null,null,null,13, 14,15, 16, 17, 18],
+                     [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36],
+                     [37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54],
+                     [55, 56, 57, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86],
+                     [87, 88, 89, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118],
+                     [null,null,null, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70,71,null],
+                     [null,null,null,90,91,92,93,94,95,96,97,98,99,100,101,102,103,null]];
 
 
 
@@ -60,23 +69,19 @@ export function Coord(x,y,z) {
 function buildTable() {
   //periodic table 2d array
   let rows = [];
-
-  //Atomic number currently screwed up due to weird thing
-  var atomicNumber = 1;
+  
   for(var row=0; row<atomicSymbols.length; row++) {
     let cell = [];
     for(var col=0; col<atomicSymbols[row].length; col++){
 
       //create new atom
       var atom = {}//new Object();
+
+      //assign physical attributes
       atom.symbol = atomicSymbols[row][col];
       atom.radius = atomicRadii[row][col];
-      if(atom.symbol !== '') {
-        atom.number = atomicNumber;
-        atomicNumber++;
-      } else {
-        atom.number = '';
-      }
+      atom.number = atomicNumber[row][col];
+
       //TODO add atomic weight, atomic category
       if(atom.number === '') {
         atom.color = "#FFFFFF";
