514c045381d45ac1b425ec1a76f95bcc12cf3685
William Jung <williamyjung@gmail.com>
2018-03-16 16:17:14 -0700

    Fixed typos in about us

--- client/components/About.js
+++ client/components/About.js
@@ -52,23 +52,23 @@ class About extends React.Component {
         students who wish to see what is in their textbooks, we believe people from any
         academic background, and of any age, will enjoy using our web app.</p> <br />
         <p align="center">If you set foot into any Chemistry student's room, you are likely
-        to see a "Molecular Model Kit". While these kits are fun to play with, they have
+        to see a "Molecular Model Kit." While these kits are fun to play with, they have
         some severe limitations. The biggest of which, is that not every molecule is
         creatable with these physical kits. By bringing software into the mix, we allow
-        for an enormous range of possibilities: "No molecule left unseen". As our codebase
+        for an enormous range of possibilities: "No molecule left unseen." As our codebase
         grows we will be able to verify users molecules as they create them, provide info about common
         molecules, show attributes like atom size, bond length, electron cloud density, etc.,
-        all of which are impossible with a physical kit. In this way our web app will
-        actively provide the user with important data about a molecule; facilitating those who use it, 
-        in their quest for a deeper understanding. As anyone who knows chem will tell you, understanding
-        the actual, physicl shape of molecules is vital in hypothesizing about their interactions. 
+        all of which are impossible with a physical kit. In this way, our web app will
+        actively provide the user with important data about a molecule, facilitating those who use it 
+        in their quest for a deeper understanding. As anyone who is learning chemistry will tell you, understanding
+        the actual, physical shape of molecules is vital in hypothesizing about their interactions. 
         We aim to help with that.</p> <br />
         <p align="center">You need not look any farther than our logo to understand our mission. Based off
-         the chemical formula of silicon dioxide, the main ingredient in computer chips, it 
-         embodies our dedication to bringing the power of 21st century computing, to the visualization,
-         and understanding, of the chemical world. </p> <br />
+         the chemical formula of silicon dioxide, the main ingredient in computer chips, our logo 
+         embodies our dedication to bringing the power of 21st century computing to the visualization
+         and understanding of the chemical world. </p> <br />
          <hr />
-         <p align="center">But enough about us, click <a href="/">here</a> or our logo to start drawing atoms and
+         <p align="center">But enough about us, click <a href="/">here</a> or on our logo to start drawing atoms and
          learning about the physical form of the world around you.</p>
         </div>
 
